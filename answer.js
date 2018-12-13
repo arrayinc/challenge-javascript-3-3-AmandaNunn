8e3fc8a171da791ba53211e17f0d563efa5646be
Amanda Nunn <amandanunn@thearrayschool.com>
2018-12-13 14:58:00 -0700

    final answer

--- answer.js
+++ answer.js
@@ -1,1 +1,14 @@
 // *** YOUR ANSWER BELOW ***
+let str = "The numbers between 1 and 100 that are divisible by 6 or 7 but not both are:";
+
+function findInts(n){
+  while(n >= 1 && n <= 100){
+    if((n % 6 === 0 && n % 7 !== 0) || (n % 7 === 0 && n % 6 !== 0)){
+      str += ` ${n},`;
+    }
+    n++;
+  }
+  return str;
+}
+
+findInts (1);
